bdfeeb32c75575a22f5481cc5129dae842940ff7
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2020-05-06 15:46:09 +0300

    MyList - Без теста

---
+++ Day 18/MyList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace Day_18
+{
+    public class MyList<T> : IEnumerable
+    {
+        private T[] list;
+        public int Count { get { return list.Length - 1; } }
+        public T this[int index]
+        {
+            get
+            {
+                return list[index];
+            }
+            set
+            {
+                list[index] = value;
+            }
+        }
+        public void Add(T item)
+        {
+            Array.Resize(ref list, list.Length + 1);
+            list[list.Length - 1] = item;
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return list.GetEnumerator();
+        }
+    }
+}
